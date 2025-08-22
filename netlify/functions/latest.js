1d6c4d5cb5c8a744a85c3cdbcbd19a344fc6e568
lololololoooooooooo <asimbeats07@gmail.com>
2025-08-22 17:59:43 +0530

    Update latest.js

--- netlify/functions/latest.js
+++ netlify/functions/latest.js
@@ -1,9 +1,10 @@
 // netlify/functions/latest.js
-// Must use the same store object as ingest.js
-const store = global.store || new Map();
+global.store = global.store || new Map();   // same store
+const store = global.store;
 
 exports.handler = async () => {
   const devices = Array.from(store.values());
+  console.log('latest returning', devices.length, 'devices');
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
